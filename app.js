5d509430f145da930ff567d23ca0df964db56335
Chahat Narwal <chahatnarwal124@gmail.com>
2023-07-05 01:56:18 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a Aws project to create a CI/CD pipeline'));
+app.get('/', (req, res) => res.send('This is a Aws project to create a CI/CD pipeline,Another commit to see continous change delivery'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
